bfa32a15d97bb22c600d1555eb78a3f775a1a7c0
mm-tvkl <mohammadmahdi.tavakoli.1384@gmail.com>
2022-03-07 10:07:00 +0330

    some Features added to Pricing Section and Pricing Card

--- js/script.js
+++ js/script.js
@@ -12,32 +12,71 @@ $('.owl-carousel').owlCarousel({
 })
 
 var switchInput = document.getElementById("flexSwitchCheckDefault");
-var price1 = document.getElementById("price1");
-var price2 = document.getElementById("price2");
-var price3 = document.getElementById("price3");
+var price1 = document.getElementById("price1") // 49$;
+var price2 = document.getElementById("price2") // 599$;
+
+var price3 = document.getElementById("price3") // 29$;
+var price4 = document.getElementById("price4") // 299$;
+
+var price5 = document.getElementById("price5") // 29$;
+var price6 = document.getElementById("price6") // 299$;
+
+// var price1 = document.getElementById("price1") // 49$;
+// var price2 = document.getElementById("price2") // 599$;
+
 var perTimes1 = document.getElementById("perTime1");
 var perTimes2 = document.getElementById("perTime2");
-var perTimes3 = document.getElementById("perTime3");
 
+var perTimes3 = document.getElementById("perTime3");
+var perTimes4 = document.getElementById("perTime4");
+
+var perTimes5 = document.getElementById("perTime5");
+var perTimes6 = document.getElementById("perTime6");
+
+var test1 = document.getElementById("test1");
+
+// Clicking Event for Switching Year or Month
 switchInput.addEventListener("click", () => {
     if (switchInput.hasAttribute("checked")) {
-        switchInput.removeAttribute("checked")
-        price1.innerHTML = "$29";
-        price2.innerHTML = "$49";
-        price3.innerHTML = "$99";
+        switchInput.removeAttribute("checked");
 
-        perTimes1.innerHTML = "Per Month";
-        perTimes2.innerHTML = "Per Month";
-        perTimes3.innerHTML = "Per Month";
+        price1.style.top = "10%";
+        price2.style.top = "200%";
+
+        price3.style.top = "10%";
+        price4.style.top = "200%";
+
+        price5.style.top = "10%";
+        price6.style.top = "200%";
+
+        perTimes1.style.top = "20%";
+        perTimes2.style.top = "200%";
+
+        perTimes3.style.top = "20%";
+        perTimes4.style.top = "200%";
+
+        perTimes5.style.top = "20%";
+        perTimes6.style.top = "200%";
     }
     else {
         switchInput.setAttribute("checked", "true");
-        price1.innerHTML = "$299";
-        price2.innerHTML = "$599";
-        price3.innerHTML = "$1,999";
 
-        perTimes1.innerHTML = "Per Year";
-        perTimes2.innerHTML = "Per Year";
-        perTimes3.innerHTML = "Per Year";
+        price1.style.top = "-100%";
+        price2.style.top = "10%";
+
+        price3.style.top = "-100%";
+        price4.style.top = "10%";
+
+        price5.style.top = "-100%";
+        price6.style.top = "10%";
+
+        perTimes1.style.top = "-100%";
+        perTimes2.style.top = "10%";
+
+        perTimes3.style.top = "-100%";
+        perTimes4.style.top = "10%";
+
+        perTimes5.style.top = "-100%";
+        perTimes6.style.top = "10%";
     }
 })
